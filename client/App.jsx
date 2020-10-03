09c070a902595e9c609f2e6ecc915b85f7acdee6
Andrew <andrew.lovato@gmail.com>
2020-10-03 17:09:17 -0400

    conflict attempt fix

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './styles.css';
 
 class App extends Component {
   constructor(){
